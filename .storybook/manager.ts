14c03bd2ba139af3d8464cbddd58a4427b93ecac
mirja-t <email@mirja-tschakarov.de>
2024-05-04 14:50:14 +0200

    add logo

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -9,17 +9,31 @@ addons.setConfig({
     fontBase: '"Fira Sans", sans-serif',
     brandTitle: 'MUI Storybook',
     brandUrl: 'https://mirja-t.github.io',
-    brandImage: '/mt-logo.gif',
-    appBg: 'white', 
+    brandImage: '/mt-logo.svg',
+
+    colorPrimary: '#e6007e',
+    colorSecondary: '#ffed00',
+
+    // Text colors
+    textColor: 'black',
+    textInverseColor: 'black',
+
+    // Toolbar default and active colors
+    barTextColor: 'black',
+    barSelectedColor: '#e6007e',
+    barHoverColor: '#e6007e',
+    barBg: '#f4f4f4',
+
+    appBg: '#e6007e', 
     appContentBg: '#f4f4f4',
     appPreviewBg: '#f4f4f4',
     appBorderColor: 'transparent',
     appBorderRadius: 0,
-    colorPrimary: '#ffed00',
-    colorSecondary: '#e6007e',
-    textColor: 'black',
-    barTextColor: 'white',
-    barBg: '#e6007e',
-    inputBorderRadius: 5,
+    
+    // Form colors
+    inputBg: '#f4f4f4',
+    inputBorder: '#CCC',
+    inputTextColor: 'black',
+    inputBorderRadius: 0,
   }),
 });
